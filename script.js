2a30d201fe256f7b678a42ffd4063ce3320a5332
Gabriel-Mamede1 <gabrielmamededosantos@outlook.com>
2022-08-09 00:17:31 -0300

    script

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
-function{
-  
-}
+document.querySelector(".hamburguer").addEventListener("click",()=>
+  document.querySelector(".container").classList.toggle("show-menu"));
+
+  document.querySelector('#qtde').addEventListener("click", atualizarPreco)
+
+  function atualizarPreco(){
+
+  }
+  const qtde = document.querySelector("qtde").nodeValue
+  console.log(qtde)
